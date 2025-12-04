66572794610c2c511c1120e4db4b717ab1fa6748
Thanugi Weerasinghe <161962010+Thanugi@users.noreply.github.com>
2025-12-04 17:58:07 +0530

    Change import of i18n from .ts to .tsx

--- ReadAndImplement/ReadAndImplement/client/src/main.tsx
+++ ReadAndImplement/ReadAndImplement/client/src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import "./i18n.ts";
+import "./i18n.tsx";
 
 createRoot(document.getElementById("root")!).render(<App />);
